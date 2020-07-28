d76c4a433d35df69f5ac087cb45b982eb6fc80f7
lawlieet <juan.1514@hotmail.com>
2020-07-28 15:25:37 -0500

    Funciones del formulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Header from './components/Header';
 
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+  max-width:600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding:3rem
+`;
+
+
 function App() {
   return (
-    <div className="App">
+    <Contenedor >
       
       <Header
          titulo="Cotizador de Seguros"
       />
-    </div>
+
+    <ContenedorFormulario>
+      <Formulario/>
+    </ContenedorFormulario>
+
+
+    </Contenedor>
   );
 }
 
